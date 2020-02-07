ac601c2b0159967d8f9e13ce11fce5a5dbf16f8a
jhon <1871901600@qq.com>
2020-02-07 22:11:27 +0800

    zhiyiblog前端页面完成

--- src/main/java/cn/zhiyigo/pblog/Dao/BannerDao.java
+++
@@ -1,8 +0,0 @@
-package cn.zhiyigo.pblog.Dao;
-
-import cn.zhiyigo.pblog.Model.Banner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BannerDao extends JpaRepository<Banner,Integer> {
-
-}
--- src/main/java/cn/zhiyigo/pblog/Dao/SiteDataDao.java
+++
@@ -1,9 +0,0 @@
-package cn.zhiyigo.pblog.Dao;
-
-import cn.zhiyigo.pblog.Model.SiteData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface SiteDataDao extends JpaRepository<SiteData,Integer> {
-
-}
